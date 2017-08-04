3c6977bb12debb182492b07a9a078814f39b5526
Lauro Moura <lauromoura@expertisesolutions.com.br>
2017-08-04 00:29:11 -0300

    eolian_mono: Add test for handling values by-value

--- src/tests/efl_mono/ValueEolian.cs
+++ src/tests/efl_mono/ValueEolian.cs
@@ -77,6 +77,38 @@ public static class TestEinaValueEolian {
         }
     }
 
+    public static void TestEolianEinaValueInReturnByValue()
+    {
+        test.Testing obj = new test.TestingConcrete();
+
+        using (eina.Value v = new eina.Value(eina.ValueType.Int32)) {
+            v.Set(42);
+            obj.set_value(v);
+            Test.AssertEquals(eina.ValueOwnership.Managed, v.Ownership);
+
+            // Using get_value_ptr while get_value() is not supported.
+            eina.Value v_received = obj.get_value_ptr_own();
+            Test.AssertEquals(eina.ValueOwnership.Managed, v_received.Ownership);
+            Test.AssertEquals(v, v_received);
+            v_received.Dispose();
+        }
+    }
+
+    public static void TestEolianEinaValueOutByValue()
+    {
+        test.Testing obj = new test.TestingConcrete();
+
+        using (eina.Value v = new eina.Value(eina.ValueType.String)) {
+            eina.Value v_out = null;
+
+            v.Set("hello!");
+            obj.set_value(v);
+            obj.out_value(out v_out);
+
+            Test.AssertEquals(v, v_out);
+            Test.AssertEquals(eina.ValueOwnership.Managed, v_out.Ownership);
+        }
+    }
 }
 #pragma warning restore 1591
 }
