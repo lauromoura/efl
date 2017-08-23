afec468f49f8d9f385642a657877102fc741b540
Taehyub Kim <taehyub.kim@samsung.com>
2017-08-23 15:50:18 +0900

    Efl.Ui.Popup.Alert: Added initial code for Efl.Ui.Popup.Alert
    Summary:
    Added initial code for Efl.Ui.Popup.Alert,
    It supports setting title and buttons
    
    Test Plan: 1. run elementary_test -to efluipopupalert
    
    Reviewers: Jaehyun_Cho, jpeg, cedric, thiepha, Blackmole, woohyun
    
    Differential Revision: https://phab.enlightenment.org/D5108

--- src/bin/elementary/test.c
+++ src/bin/elementary/test.c
@@ -269,6 +269,7 @@ void test_datetime(void *data, Evas_Object *obj, void *event_info);
 void test_ui_clock(void *data, Evas_Object *obj, void *event_info);
 void test_popup(void *data, Evas_Object *obj, void *event_info);
 void test_efl_ui_popup(void *data, Evas_Object *obj, void *event_info);
+void test_efl_ui_popup_alert(void *data, Evas_Object *obj, void *event_info);
 void test_efl_ui_popup_scroll(void *data, Evas_Object *obj, void *event_info);
 void test_dayselector(void *data, Evas_Object *obj, void *event_info);
 void test_image(void *data, Evas_Object *obj, void *event_info);
@@ -954,6 +955,7 @@ add_tests:
    ADD_TEST(NULL, "Popups", "Tooltip 4", test_tooltip4);
    ADD_TEST(NULL, "Popups", "Popup", test_popup);
    ADD_TEST(NULL, "Popups", "Efl UI Popup", test_efl_ui_popup);
+   ADD_TEST(NULL, "Popups", "Efl UI Popup Alert", test_efl_ui_popup_alert);
    ADD_TEST(NULL, "Popups", "Efl UI ScrollPopup", test_efl_ui_popup_scroll);
 
    //------------------------------//
