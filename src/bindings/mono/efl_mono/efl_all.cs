c6a98bbba11b443159da5416e32f0f121d5d1955
Lauro Moura <lauromoura@expertisesolutions.com.br>
2017-10-11 18:35:15 -0300

    efl_mono: Add initial support code for elm
    Init/shutdown/run/exit

--- src/bindings/mono/efl_mono/efl_all.cs
+++ src/bindings/mono/efl_mono/efl_all.cs
@@ -18,6 +18,7 @@ public class Libs {
     public const string Evas = "libevas-1";
     public const string Evil = "libevil-1";
     public const string EcoreEvas = "libecore_evas-1";
+    public const string Elementary = "libelementary-1";
 
     public const string CustomExports = "libeflcustomexportsmono-0";
 #else
@@ -28,6 +29,7 @@ public class Libs {
     public const string Evas = "evas";
     public const string Evil = "dl"; // Used for dlsym, dlopen and dlerror
     public const string EcoreEvas = "ecore_evas";
+    public const string Elementary = "elementary";
 
     public const string CustomExports = "eflcustomexportsmono";
 #endif
@@ -38,12 +40,17 @@ static class UnsafeNativeMethods {
     [DllImport(efl.Libs.Ecore)] public static extern void ecore_shutdown();
     [DllImport(efl.Libs.Evas)] public static extern void evas_init();
     [DllImport(efl.Libs.Evas)] public static extern void evas_shutdown();
+    [DllImport(efl.Libs.Elementary)] public static extern int elm_init(int argc, IntPtr argv);
+    [DllImport(efl.Libs.Elementary)] public static extern void elm_shutdown();
+    [DllImport(efl.Libs.Elementary)] public static extern void elm_run();
+    [DllImport(efl.Libs.Elementary)] public static extern void elm_exit();
 }
 
 public static class All {
     public static void Init() {
         eina.Config.Init();
         efl.eo.Config.Init();
+        efl.ui.Config.Init();
         ecore_init();
         evas_init();
     }
@@ -56,6 +63,7 @@ public static class All {
 
         evas_shutdown();
         ecore_shutdown();
+        efl.ui.Config.Shutdown();
         efl.eo.Config.Shutdown();
         eina.Config.Shutdown();
     }
@@ -63,6 +71,29 @@ public static class All {
     public static int Main() {
         return 0;
     }
+}
+
+// Placeholder. Will move to elm_config.cs later
+namespace ui {
+
+public static class Config {
+    public static void Init() {
+        // TODO Support elm command line arguments
+        elm_init(0, IntPtr.Zero);
+    }
+    public static void Shutdown() {
+        elm_shutdown();
+    }
+
+    public static void Run() {
+        elm_run();
+    }
+
+    public static void Exit() {
+        elm_exit();
+    }
+}
+
 }
 
 }
